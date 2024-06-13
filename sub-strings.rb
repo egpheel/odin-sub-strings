27870ad998962417d7b9fd99e64e8444f70ecf51
Eduardo Gil <egpheel@users.noreply.github.com>
2024-06-13 20:38:48 +0100

    check if string word includes substrings
    - check if the string word includes any of the substrings included in
    the dictionary array and print out said substrings if so

--- sub-strings.rb
+++ sub-strings.rb
@@ -2,4 +2,10 @@
 
 dictionary = %w[below down go going horn how howdy it i low own part partner sit]
 
-def substrings(word = '', dictionary = []); end
+def substrings(word = '', dictionary = [])
+  dictionary.each do |dict_word|
+    p dict_word if word.include?(dict_word)
+  end
+end
+
+substrings('below', dictionary)
