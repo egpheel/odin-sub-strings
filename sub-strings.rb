28e9769635640b6db26163985f7c7321ab2f1ba5
Eduardo Gil <egpheel@users.noreply.github.com>
2024-06-13 20:49:20 +0100

    added all matching substring to an array

--- sub-strings.rb
+++ sub-strings.rb
@@ -3,9 +3,14 @@
 dictionary = %w[below down go going horn how howdy it i low own part partner sit]
 
 def substrings(text = '', dictionary = [])
+  matching_substrings = []
+
   dictionary.each do |dict_word|
-    p dict_word if text.include?(dict_word)
+    # p dict_word if text.include?(dict_word)
+    matching_substrings << dict_word if text.include?(dict_word)
   end
+
+  p matching_substrings
 end
 
 substrings('below', dictionary)
